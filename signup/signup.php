131c3d2dd3325488e9d0423f5cd87b6a809fac44
Manualex98 <alessi.1795191@studenti.uniroma1.it>
2023-02-11 16:43:27 +0100

    modifiche parziali

--- signup/signup.php
+++ signup/signup.php
@@ -10,8 +10,9 @@
                     $ql=" select * from users where email= $1";
                     $result=pg_query_params($dbconn, $ql, array($email));
                     if ($line= pg_fetch_array($result, null, PGSQL_ASSOC)){
-                        echo "<h1> Sorry, you are already a registered user</h1>
-                        <a href=../login/login.html>Click here to login</a>";
+                        echo "<h1> Attenzione, esiste già un account con la tua email <br>o l'username è già stato scelto</h1>
+                        <a href=../login/login.html>Clicca qui per accedere.</a><br><br>
+                        <a href=signup.html>o qui per registrarti</a>";
                     }
                     else {
                         $name=$_POST['inputName'];
